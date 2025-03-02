be7a22c594ae9035245e042a78b7d2031155c4af
Rashid-ceme <mrashidceme@gmail.com>
2025-03-02 15:25:12 +0500

    configured docker compose and checks to wait for db

--- app/app/test.py
+++ app/app/test.py
@@ -13,7 +13,7 @@ class CalcTests(SimpleTestCase):
         """Test adding numbers together."""
         res = calc.add(5, 6)
 
-        self.assertEqual(res, 12)
+        self.assertEqual(res, 11)
 
 
 
